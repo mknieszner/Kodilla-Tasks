cae80077f202ba559237e7b010d0e799ae750ab6
mknieszner <mknieszner@gmail.com>
2017-11-01 04:33:18 +0100

    Trello mail update

---
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Admin configuration class.
+ */
+@Component
+@Getter
+public class AdminConfig {
+  @Value("${admin.mail}")
+  private String adminMail;
+}
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,6 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,25 +20,25 @@ import java.util.List;
 public class TrelloController {
 
   @Autowired
-  private TrelloClient trelloClient;
+  private TrelloService trelloClient;
 
-  @GetMapping("boardsHeaders")
-  public void printTrelloBoardsHeaders() {
-    trelloClient
-        .getTrelloBoards()
-        .stream()
-        .filter(trelloBoardDto -> StringUtils.isNotBlank(trelloBoardDto.getName()) && StringUtils.isNotBlank(trelloBoardDto.getId()))
-        .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-        .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-  }
+//  @GetMapping("boardsHeaders")
+//  public void printTrelloBoardsHeaders() {
+//    trelloClient
+//        .getTrelloBoards()
+//        .stream()
+//        .filter(trelloBoardDto -> StringUtils.isNotBlank(trelloBoardDto.getName()) && StringUtils.isNotBlank(trelloBoardDto.getId()))
+//        .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
+//        .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+//  }
 
   @GetMapping("boards")
   public List<TrelloBoardDto> getTrelloBoards() {
-    return trelloClient.getTrelloBoards();
+    return trelloClient.fetchTrelloBoards();
   }
 
   @PostMapping("card")
   public CreatedTrelloCard createTrelloCard(@RequestBody final TrelloCardDto trelloCardDto) {
-    return trelloClient.createNewCard(trelloCardDto);
+    return trelloClient.createTrelloCard(trelloCardDto);
   }
 }
