fe45170cadde30e1e947ce18b795fc359595159e
mknieszner <mknieszner@gmail.com>
2017-11-16 23:16:41 +0100

    21.3
    First Commit

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -1,116 +1,140 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.repository.TaskRepository;
-import com.crud.tasks.service.TaskNotFoundException;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.junit.Assert;
+import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.util.NestedServletException;
 
 
-import java.io.IOException;
 import java.nio.charset.Charset;
-import java.sql.*;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- *
+ * Test Suite for Task Controller.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TaskControllerTest {
-
-  @Autowired
-  TaskRepository taskRepository;
-
+  @MockBean
+  private DbService dbService;
   @Autowired
   private MockMvc mockMvc;
-//
+
   @Test
   public void getTasksTest() throws Exception {
-    mockMvc.perform(
-        get("/v1/tasks/"))
+    //Given
+    final List<Task> tasks = new ArrayList<>();
+    tasks.add(new Task(1L, "test_title", "test_content"));
+    when(dbService.getAllTask()).thenReturn(tasks);
+
+    //When & Then
+    mockMvc.perform(get("/v1/tasks/"))
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(content().string(containsString("\"title\":\"\",\"content\":\"\"}")));
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].id", is(tasks.get(0).getId().intValue())))
+        .andExpect(jsonPath("$[0].title", is(tasks.get(0).getTitle())))
+        .andExpect(jsonPath("$[0].content", is(tasks.get(0).getContent())));
   }
 
-//  @Test
-//  public void getTaskTest() throws Exception {
-//    mockMvc.perform(
-//        get("/v1/tasks/2"))
-//        .andDo(print())
-//        .andExpect(status().isOk())
-//        .andExpect(content()
-//            .string(containsString("{\"id\":2,\"title\":\"\",\"content\":\"\"}")));
-//  }
+  @Test
+  public void getTaskTest() throws Exception {
+    //Given
+    final Task testTask = new Task(1L, "test_title", "test_content");
+    when(dbService.findById(1)).thenReturn(testTask);
 
-//  @Test
-//  public void updateTaskTest() throws Exception {
-//    mockMvc.perform(put("/v1/tasks/2")
-//        .contentType(TestUtil.APPLICATION_JSON_UTF8)
-//        .content(TestUtil.convertObjectToJsonBytes(new TaskDto((long) 2, "", ""))))
-//        .andDo(print()).andExpect(content().string(containsString("{\"id\":2,\"title\":\"\",\"content\":\"\"}"))).andExpect(status().isOk());
-//  }
+    //When & Then
+    mockMvc.perform(get("/v1/tasks/" + testTask.getId()))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is(testTask.getId().intValue())))
+        .andExpect(jsonPath("$.title", is(testTask.getTitle())))
+        .andExpect(jsonPath("$.content", is(testTask.getContent())));
+
+  }
+
+  @Test
+  public void updateTaskTest() throws Exception {
+    //Given
+    final Task testTask = new Task(1L, "test_title", "test_content");
+    when(dbService.saveTask(ArgumentMatchers.any(Task.class))).thenReturn(testTask);
+
+    //When & Then
+    mockMvc.perform(put("/v1/tasks/" + testTask.getId())
+        .contentType(APPLICATION_JSON_UTF8)
+        .content(toJson(testTask)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is(testTask.getId().intValue())))
+        .andExpect(jsonPath("$.title", is(testTask.getTitle())))
+        .andExpect(jsonPath("$.content", is(testTask.getContent())));
+  }
 
   @Test
   public void deleteTaskTest() throws Exception {
     //Given
-    final Task toDeleteTask = taskRepository.save(new Task(null, "", ""));
+    doNothing().when(dbService).deleteTask(ArgumentMatchers.any(Long.class));
 
     //When Then
-    mockMvc.perform(
-        delete("/v1/tasks/" + toDeleteTask.getId()))
+    mockMvc.perform(delete("/v1/tasks/1"))
         .andDo(print())
         .andExpect(status().isOk());
   }
 
   @Test
   public void createTaskTest() throws Exception {
-    mockMvc.perform(
-        post("/v1/tasks/")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(new TaskDto(null, "", ""))))
+    //Given
+    final Task testTask = new Task(1L, "test_title", "test_content");
+    when(dbService.saveTask(ArgumentMatchers.any(Task.class))).thenReturn(testTask);
+
+    //When & Then
+    mockMvc.perform(post("/v1/tasks/")
+        .contentType(APPLICATION_JSON_UTF8)
+        .content(toJson(testTask)))
         .andDo(print())
         .andExpect(status().isOk());
   }
 
   @Test(expected = NestedServletException.class)
   public void taskNotFoundExceptionTest() throws Exception {
+    //Given & When
     mockMvc.perform(get("/v1/tasks/-1"));
 
+    //Then
     // Exception should be thrown
   }
 
-private static class TestUtil {
-  public static final MediaType APPLICATION_JSON_UTF8 =
-      new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-  public static byte[] convertObjectToJsonBytes(final Object object) throws IOException {
-    final ObjectMapper mapper = new ObjectMapper();
-    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    return mapper.writeValueAsBytes(object);
+  private String toJson(final Object o) {
+    final Gson gson = new Gson();
+    return gson.toJson(o);
   }
+
+  private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+      MediaType.APPLICATION_JSON.getType(),
+      MediaType.APPLICATION_JSON.getSubtype(),
+      Charset.forName("utf8"));
 }
-}
+
+
+
--- src/test/java/com/crud/tasks/controller/TrelloControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTestSuite.java
@@ -1,62 +1,119 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.trello.facade.TrelloFacade;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static jdk.nashorn.internal.runtime.JSType.isString;
+//import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.ResultMatcher.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Test Suite for Trello Controller.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TrelloController.class)
 public class TrelloControllerTestSuite {
-
-  @Autowired
-  TaskRepository taskRepository;
 
   @Autowired
   private MockMvc mockMvc;
 
+  @MockBean
+  private TrelloFacade trelloFacade;
+
   @Test
-  public void getTasksTest() throws Exception {
-    mockMvc.perform(get("/v1/trello/boards")).andDo(print()).andExpect(status().isOk());
+  public void shouldFetchEmptyTrelloBoards() throws Exception {
+    //Given
+    final List<TrelloBoardDto> trelloBoardsDto = new ArrayList<>();
+    when(trelloFacade.fetchTrelloBoards()).thenReturn(trelloBoardsDto);
+
+    //When & Then
+    mockMvc.perform(get("/v1/trello/boards")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(0)));
   }
 
   @Test
-  public void createTrelloCardTest() throws Exception {
-    mockMvc.perform(
-        post("/v1/tasks/")
+  public void shouldFetchTrelloBoards() throws Exception {
+    final List<TrelloListDto> trelloLists = new ArrayList<>();
+    trelloLists.add(new TrelloListDto("1", "test_list", false));
+
+    final List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+    trelloBoards.add(new TrelloBoardDto("test_name", "1", trelloLists));
+
+    when(trelloFacade.fetchTrelloBoards()).thenReturn(trelloBoards);
+
+    //When & Then
+    mockMvc.perform(get("/v1/trello/boards")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        // Trello Boards fields
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].id", is("1")))
+        .andExpect(jsonPath("$[0].name", is("test_name")))
+        // Trello List fields
+        .andExpect(jsonPath("$[0].lists", hasSize(1)))
+        .andExpect(jsonPath("$[0].lists.[0].id", is("1")))
+        .andExpect(jsonPath("$[0].lists.[0].name", is("test_list")))
+        .andExpect(jsonPath("$[0].lists.[0].closed", is(false)));
+  }
+
+  @Test
+  public void shouldCreateTrelloCard() throws Exception {
+    //Given
+    final TrelloCardDto trelloCardDto = new TrelloCardDto("Test", "Test_Description", "top", "1");
+    final CreatedTrelloDto createdTrelloDto = new CreatedTrelloDto("123", "Test", "http//test.com", null);
+    when(trelloFacade.createCard(ArgumentMatchers.any(TrelloCardDto.class))).thenReturn(createdTrelloDto);
+    final Gson gson = new Gson();
+    final String jsonContent = gson.toJson(trelloCardDto);
+
+    //When & Then
+    mockMvc.perform(post("/v1/trello/card")
         .contentType(TrelloControllerTestSuite.TestUtil.APPLICATION_JSON_UTF8)
-            .content(TrelloControllerTestSuite.TestUtil.convertObjectToJsonBytes(getTrelloCardDto())))
-        .andDo(print())
-        .andExpect(status().isOk());
+        .characterEncoding("UTF-8").content(jsonContent))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is("123")))
+        .andExpect(jsonPath("$.name", is("Test")))
+        .andExpect(jsonPath("$.shortUrl", is("http//test.com")));
   }
 
   private static class TestUtil {
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
         MediaType.APPLICATION_JSON.getType(),
-        MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+        MediaType.APPLICATION_JSON.getSubtype(),
+        Charset.forName("utf8"));
 
     public static byte[] convertObjectToJsonBytes(final Object object) throws IOException {
       final ObjectMapper mapper = new ObjectMapper();
